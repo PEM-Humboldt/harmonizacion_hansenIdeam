32a1d989a893d4daba335781cb696bf75168646f
Jeronimo Rodriguez <jeronimo.rodriguez@temple.edu>
2024-06-17 17:46:15 -0400

    Updated version to download the data. Testing in nimbus because of memory limit locally.

--- R/script_dwnld.R
+++ R/script_dwnld.R
@@ -1,18 +1,13 @@
 
-                                        # Hansen forest  Map Downloader Using Ecochange and determining threshold from an attribute table. 
+# Hansen forest  Map Downloader Using Ecochange and determining threshold from an attribute table. 
 
-setwd("~/Documents/biomas_iavh/Matrices_Abril") #keep in mind for the documentation, remove after)
+setwd("~/Documents/biomas_iavh/Final_results_Codename_Abril") #keep in mind for the documentation, remove after)
 #setwd('/Users/sputnik/Documents/biomas_iavh/Biomas IAvH')
 packs <- c('terra', 'raster','parallel', 'R.utils', 'rvest','xml2','tidyverse', 'landscapemetrics', 'sf','dplyr','httr','getPass',
            'rasterVis','rlang', 'rasterDT', 'ecochange')
 
 #out.. <-  sapply(packs, install.packages, character.only = TRUE)
 out.. <-  sapply(packs, require, character.only = TRUE)
-
-#gdalUtils::gdal_setInstallation()
-#valid_install <- !is.null(getOption("gdalUtils_gdalPath"))
-setwd('~/Forest_Armonization/FINAL_HANSEN/output_armonization')
-
 
 #load Vector Data ROI
            # It uses the attribute table to extract data labeling and parameter definition information (name spatial unit) and splits in the different 
@@ -21,12 +16,11 @@ masked <- st_read('biomes_attributes_msk.shp')
 #masked <- st_read('/Users/sputnik/Documents/biomas_iavh/Final_results_Codename_Abril/biomes_attributes_msk.shp')
 masked <- st_transform(masked,crs=4326)
 
-
 #here, you need to filter and select the biome you need. You can filter sf objects with tidyverse)
 masked <- masked%>%subset(!is.na(accurcy))
 labels <- (masked$biome)
 
-masked <- as(masked, 'Spatial')
+#masked <- as(masked, 'Spatial')
 # split mun into list of independent polygons. You don't need it 
 #biomat <- masked%>%split(.$biome)
 
@@ -34,12 +28,22 @@ biomat <- masked%>%split(.$biome)
            #Run individual example (documentation R)
 
 
+def <- lapply(biomat, function(sf){
+  d <- echanges(sf,
+                lyrs = c('treecover2000','lossyear'),
+                path = getwd(),
+                eco_range = c(sf$threshld,100),
+                change_vals = seq(22,23,1),
+                mc.cores = 4) 
+                })
+
+
 suppressWarnings(
   def <- echanges(test,   # polígono 
                   lyrs = c('treecover2000','lossyear'),      # nombres de las capas
                   path = getwd(),      # directorio de trabajo, en caso de que no desees trabajar en el directorio temporal
                   eco_range = c(test$thrshld,100),      # Umbral de treecover2000
-                  change_vals = seq(19,21,1),      # en este caso, los años de pérdida, 
+                  change_vals = seq(21,22,1),      # en este caso, los años de pérdida, 
                   mc.cores = 9)     # número de núcleos, solo funciona en sistema linux
 )
 
